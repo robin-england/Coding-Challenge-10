10bbbdaace3b8455afc1426efa18285e7cdb80a3
robin-england <robin.england94@googlemail.com>
2024-06-15 17:54:59 -0400

    Finalize data display functionality.

--- example.js
+++ example.js
@@ -1,13 +1,13 @@
 function main(){    // runs on html body loading
 
-    d3.csv('purchase_orders.csv',d3.autoType).then( //pulls CSV file and then runs function
+    d3.dsv(',','purchase_orders.csv',d3.autoType).then( //pulls CSV file and then runs function
     function(d){
         for (let index = 0; index < d.length; index++) {    //pull each line one by one
             const element = d[index];                       //creates an element for line from CSV file
-            d3.select('uL')
-                .append('li')                               
-                .text(element.customerName + " - " + element.orderId + ": $" + element.purchaseAmount);
+            d3.select('uL')                                 //selects list in html file
+                .append('li')                               //adds list item for each element
+                .text(element.customerName + " - " + element.orderId + ": $" + element.purchaseAmount); //adds text to each list item
+            }
         }
-    }
-)
+    )
 }
