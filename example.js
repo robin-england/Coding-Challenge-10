615af4eaaf5406e196ec8d7865fcb96c296d64c6
robin-england <robin.england94@googlemail.com>
2024-06-15 17:51:08 -0400

    Implement data loading and DOM manipulation with D3.js.

--- example.js
+++ example.js
@@ -1,3 +1,13 @@
-function main(){
+function main(){    // runs on html body loading
 
+    d3.csv('purchase_orders.csv',d3.autoType).then( //pulls CSV file and then runs function
+    function(d){
+        for (let index = 0; index < d.length; index++) {    //pull each line one by one
+            const element = d[index];                       //creates an element for line from CSV file
+            d3.select('uL')
+                .append('li')                               
+                .text(element.customerName + " - " + element.orderId + ": $" + element.purchaseAmount);
+        }
+    }
+)
 }
